2558fab1d2d997afa99cccf14eb1f849545395d7
vladikkiller <71603112+vladikkiller@users.noreply.github.com>
2022-10-19 21:20:42 +0300

    Add project files.

---
+++ Notes/Notes/AppShell.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Notes.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Notes
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
